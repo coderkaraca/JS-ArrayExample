c94a36e4a99cf70e53781b4efa67d0374cec38b9
Gökhan KARACA <90473208+coderkaraca@users.noreply.github.com>
2023-04-25 09:18:45 +0300

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -9,19 +9,19 @@
 </head>
 
 <body>
-    <form onsubmit="return deneme()">
+    <form onsubmit="return diziEkle()">
         <h3>Diziye eklemek istediğiniz indeksi yazınız</h3>
-        <input type="text" id="ekle" name="ekle">
+        <input type="text" id="ekle" name="ekle" placeholder="Diziye eklenecek değer">
         <input type="submit" value="Dizi Ekle">
     </form>
     <script type="text/javascript" name="javasccript">
-        let dizi = []; // Oluşturduğumuz dizi, eklenecek olan indekslerin devam etmesi için fonksiyon dışında olmalı.
+        const dizi = []; // Oluşturduğumuz dizi, eklenecek olan indekslerin devam etmesi için fonksiyon dışında olmalı.
 
-        function deneme() {
-            dizilistesi = document.getElementById('ekle').value; // ekle ID'li input bölümüne ulaşıyoruz.
+        function diziEkle() {
+            const dizilistesi = document.getElementById('ekle').value; // ekle ID'li input bölümüne ulaşıyoruz.
             dizi.push(dizilistesi); // Dizi listesine girilen indeks değeri dizi içerisine dahil edilir.
             console.log(dizi); // Dizi içeriğini console sayfasından anlık izleyebiliriz.
-            return false; // Gönderimi durdur.
+            event.preventDefault(); // Gönderimi durdur.
         }
     </script>
 </body>
